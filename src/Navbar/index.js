f4e196d21f0ed6492442e115f833238784e96c06
the devil himself <danielfangzhu@gmail.com>
2022-07-13 14:43:35 -0700

    replaced vanilla bootstrap with react-bootstrap

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -2,18 +2,20 @@ import logo from '../invert_santa.png';
 import './index.css';
 import React from 'react';
 import {Link} from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navb from 'react-bootstrap/Navbar';
 function Navbar() {
-    return <nav className="navbar navbar-dark bg-dark navbar-expand">
-        <a className="navbar-brand" href="#">
+    return <Navb bg="dark" expand="lg" variant="dark">
+        <Navb.Brand as={Link} to="/">
             <img src={logo} width="100" height="100" alt="" style={{ borderRadius: '50%', marginLeft: '20px' }}></img>
-        </a>
-        <div className="navbar-collapse collapse">
-            <ul className='navbar-nav ms-auto'>
-                <li className="nav-item"><Link className="nav-link" to='/'>Home</Link></li>
-                <li className="nav-item"><Link className="nav-link" to='/about'>About me</Link></li>
-                <li className="nav-item"><Link className="nav-link" to='/contact'>Contact me</Link></li>
-            </ul>
-        </div>
-    </nav>
+        </Navb.Brand>
+        <Navb.Toggle aria-controls="basic-navbar-nav" />
+        <Navb.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav>
+        </Navb.Collapse>
+    </Navb>
 }
 export default Navbar;
